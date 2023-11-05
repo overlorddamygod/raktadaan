679a1e16d119daf5e4fce28abfa228e33d8131e2
Overlord <overlord.damygod@gmail.com>
2023-11-05 16:15:41 +0545

    fix: multi file exports

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,7 +1,7 @@
-export 'package:raktadaan/src/widgets/button.dart';
-export 'package:raktadaan/src/widgets/icon_button.dart';
-export 'package:raktadaan/src/widgets/blood_type_form_select.dart';
-export 'package:raktadaan/src/widgets/verified.dart';
-export 'package:raktadaan/src/widgets/text_input.dart';
-export 'package:raktadaan/src/widgets/password_input.dart';
-export 'package:raktadaan/src/widgets/home_icon_button.dart';
+export 'button.dart';
+export 'icon_button.dart';
+export 'blood_type_form_select.dart';
+export 'verified.dart';
+export 'text_input.dart';
+export 'password_input.dart';
+export 'home_icon_button.dart';
